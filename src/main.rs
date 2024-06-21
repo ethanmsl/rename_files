4c4ac73e1bdb5d4c73c1e6369c150f6e7bc99c03
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2024-06-21 12:40:17 -0400

    Feat: string name replace and flagging syntax

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 //! CLI interface to allow regex based file renaming
+//!
+//! # Example:
+//! ```bash
+//! clear; el; carr -- --regex '(C|c)argo.*(\..*)' --replacement '$1ogra$2'
+//! ```
 
 // Notes:
 // Show files that can and can't be written to.
 // Show files that do and don't match patterns (?)
 // Have limits on what's shown?
+
+use std::fs::rename;
 
 use clap::Parser;
 use itertools::Itertools;
@@ -13,6 +20,11 @@ use rename_files::{error::Result, logging::tracing_subscribe_boilerplate};
 use tracing::{error, info, warn};
 use walkdir::WalkDir;
 
+// regex for checking a reference number followed by other chars
+// e.g. `$1abc` will be parsed as ($1abc) NOT ($1)(abc)
+//      `${1}abc` is proper syntax
+const RE_SYNTAX_WARN: &str = r"(\$\d)[^\d\$]+";
+
 /// CLI input arguments
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -20,6 +32,10 @@ struct Args {
     /// like element to search for in subdomain names
     #[arg(long)]
     regex: String,
+
+    /// like element to search for in subdomain names
+    #[arg(long)]
+    replacement: Option<String>,
 
     /// recurse into child directories
     #[arg(short, long)]
@@ -41,6 +57,18 @@ fn main() -> Result<()> {
     let args = Args::parse();
     // translate to regex
     let re = Regex::new(&args.regex)?;
+
+    // Flagging unintended syntax
+    if let Some(rep_arg) = &args.replacement {
+        let re_check = Regex::new(RE_SYNTAX_WARN).expect("valid, static regex");
+        tracing::info!("{:?}", &rep_arg);
+        tracing::info!("{:?}", &re_check);
+        re_check.captures_iter(rep_arg).for_each(|cap| {
+                                           tracing::warn!("\nWarning: capture reference `{}` is being read as `{}`\nIf this is not intended use: `${{_}}...` instead.",
+                                                          cap[1].to_string().blue(),
+                                                          cap[0].to_string().red());
+                                       });
+    }
 
     // Recurse?
     let walkable_space = if args.recurse {
@@ -71,6 +99,13 @@ fn main() -> Result<()> {
 
         println!("for regex '{}' a match was found in entry {}", &args.regex.green(), &entry.purple());
         println!("The captures are: {:?}", &captures.blue());
+
+        if let Some(rep) = &args.replacement {
+            tracing::warn!(entry);
+            tracing::warn!(rep);
+            let new_name = re.replace(entry, rep);
+            println!("The new name is: {}", &new_name.red());
+        }
     }
 
     // if --change-yes
