59ad07b4685b68c57da08a61ecfe733beb3c5e48
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2024-06-20 22:18:05 -0400

    Feat: early error and logging boilerplate

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,29 @@
+/// Very early error type that can flow into standard error handling with type coercions.
+pub mod error {
+    pub type Result<T> = core::result::Result<T, Error>;
+    pub type Error = Box<dyn std::error::Error>;
+}
+
+/// Simple logging boilerplate code.
+pub mod logging {
+    use tracing_subscriber::EnvFilter;
+
+    pub fn tracing_subscribe_boilerplate(env_min: impl Into<String>) {
+        let filter = EnvFilter::try_new(
+                    std::env::var("RUST_LOG").unwrap_or_else(|_| env_min.into()),
+                )
+                .expect("Valid filter input provided.");
+
+        tracing_subscriber::fmt().pretty()
+                                 .with_env_filter(filter)
+                                 .with_file(true)
+                                 .with_line_number(true)
+                                 .with_thread_ids(true)
+                                 .with_target(true)
+                                 .init();
+    }
+}
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
