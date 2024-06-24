284da7e47b094e992db64de9b1d1e7772524051b
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2024-06-24 15:35:03 -0700

    Chore: unused code resevoir
    Not a common or popular approach.  But discarding code or
    un-ergonomically hiding behind branches seems unwise to me.
    Often useful when revisiting an old repo to see snippets of relevant and
    potentially useful code.

---
+++ src/unused.rs
@@ -0,0 +1,19 @@
+//! Bits 'n Bobs box.
+//! Unused code snippets that may be useful later.
+
+/// Checks for a literal `.` prefix on a entry
+///
+/// # Unused Because:o
+/// Already have general regex search.  May implement if need an ergonomic 'quick path'.
+///
+/// # Note: This will trigger on the `.` used to indicate the 'local' directory
+#[allow(unused)]
+fn is_hidden(entry: &walkdir::DirEntry) -> bool {
+    let is_hidden = entry.file_name().to_str().map(|s| s.starts_with('.')).unwrap_or(false);
+    if is_hidden {
+        tracing::trace!("Ignoring hidden file: {:?}", entry.path());
+    } else {
+        tracing::trace!("Not a hidden file: {:?}", entry.path());
+    }
+    is_hidden
+}
