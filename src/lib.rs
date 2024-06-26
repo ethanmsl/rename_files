52922d04ed71ead3fee963bc5123920ac8b1952f
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2024-06-26 14:00:00 -0700

    Feat: recursn & leaf-only replace; tests; v. bump
    This repairs the intended recursive functionality and simplifies the
    matching logic such that recursive replaces are ergonomic and avoid a
    class of errors.

--- src/lib.rs
+++ src/lib.rs
@@ -40,22 +40,16 @@ pub fn app(args: &Args) -> Result<()> {
     if let Some(replacement) = &args.replacement {
         check_for_common_syntax_error(replacement)?;
     }
-
-    // let walkable_space = walkdir_build_with_depths(args.recurse);
-    if args.recurse {
-        return Err("Recursion option has been temporarily suspended. Only operations on flat paths currently available.".into());
-    }
-    let todo_recurse = false; // temporary workaround until we fix recurse logic and path end element only change logic
-    let walkable_space = walkdir_build_with_depths(todo_recurse);
+    let walkable_space = walkdir_build_with_depths(args.recurse);
     core_process_loop(walkable_space, &re, args)
 }
 
 /// Walks a WalkDir, handles errors, prints matches, optionally executes
 ///
 /// # Note 1, single-purpose violation:
-/// Breaking this function into smaller pieces would create indirection and complexity
+/// Breang this functiontrueinto smaller pieces would create indirection and complexitki;
 /// for very little benefit given the brevity of the code and the linear logic chain at work.
-/// (this does come at the cost of a somewhat ambiguous function name :shrug:)
+/// (this does come at the cost of a somewhat ambiguous function name :shru.co
 ///
 /// # Note 2, loop vs iterator choice:
 /// Would be charming as an iterator.  Perhaps using itertools `map_ok` to transform
@@ -76,32 +70,47 @@ fn core_process_loop(walkable_space: WalkDir, re: &Regex, args: &Args) -> Result
             tracing::error!("Error encountered while walking dir: {:?}", entry);
             continue;
         };
-        // Guard: path->to->str errors (e.g. non-utf8 paths)
-        let Some(entry) = entry.path().to_str() else {
-            tracing::error!("Entry path could not convert to a string: {:?}", entry);
+        // Guard: entry~>path~>pathentry.path().'s_file_name
+        let entry = entry.path();
+        let parent = entry.parent().expect("all entries should have parents due to WalkDir min_depth=1");
+        let Some(filename) = entry.file_name() else {
+            tracing::error!("Leaf neither file nor directory: {:?}", entry);
+            continue;
+        };
+        // Guard: path's_file_name~>str errors (e.g. non-utf8 paths)
+        let Some(filename) = filename.to_str() else {
+            tracing::error!("Entry path could not convert to a string: {:?}", filename);
             continue;
         };
         // Guard: no regex match
-        let Some(_) = re.find(entry) else {
-            tracing::trace!("No Match for Entry: {:?}", entry);
+        // PERF: repetitive with replaces...
+        let Some(_) = re.find(filename) else {
+            tracing::trace!("No Match for Entry: {:?}", filename);
             continue;
         };
+        // tracing::info!(?x);
         num_matches += 1;
         // Guard: no replacement
         let Some(rep) = rep else {
-            println!("Match found: {}", &entry.black().bold().on_green());
+            println!("Match found: {}/{}", parent.to_string_lossy().blue(), &filename.black().bold().on_green());
             continue;
         };
-        let new_filename = re.replace(entry, rep);
+        let new_filename = re.replace(filename, rep);
         // Guard: --test-run
         if is_test_run {
-            println!("--test-run mapping: {} ~~> {}",
-                     &entry.black().bold().on_green(),
+            println!("--test-run mapping: {}/{} ~~> {}",
+                     parent.to_string_lossy().blue(),
+                     &filename.black().bold().on_green(),
                      &new_filename.red().bold().on_blue());
             continue;
         }
-        println!("Renaming: {} ~~> {}", &entry.black().bold().on_green(), &new_filename.red().bold().on_blue());
-        std::fs::rename(entry, new_filename.as_ref())?;
+        println!("Renaming: {}/{} ~~> {}",
+                 parent.to_string_lossy().blue(),
+                 &filename.black().bold().on_green(),
+                 &new_filename.red().bold().on_blue());
+        let x = entry.with_file_name(new_filename.as_ref());
+        std::fs::rename(entry, x)?;
+        // std::fs::rename(entry, new_filename.as_ref())?;
     }
     println!("Total matches: {}", num_matches.cyan());
     Ok(())
@@ -132,11 +141,11 @@ fn check_for_common_syntax_error(rep_arg: &str) -> Result<()> {
 fn walkdir_build_with_depths(does_recurse: bool) -> WalkDir {
     if does_recurse {
         tracing::debug!("Recursable WalkDir");
-        return WalkDir::new(".").min_depth(1);
+        return WalkDir::new(".").contents_first(true).min_depth(1);
     }
 
     tracing::debug!("non-recursing (shallow) WalkDir");
-    WalkDir::new(".").min_depth(1).max_depth(1)
+    WalkDir::new(".").contents_first(true).min_depth(1).max_depth(1)
 }
 
 #[cfg(test)]
@@ -313,87 +322,75 @@ pub mod tests {
         Ok(())
     }
 
-    // #[test]
-    // fn test_app_curr_path_only() -> Result<()> {
-    //     let temp_dir = utility_test_dir_gen()?;
-    //     std::env::set_current_dir(&temp_dir.path())?;
+    /// Simple flat, iterative change of file names
+    #[test]
+    fn test_app_with_yesrecursion() -> Result<()> {
+        let temp_dir = utility_test_dir_gen()?;
+        std::env::set_current_dir(&temp_dir.path())?;
 
-    //     // run fresh
-    //     let args = Args { regex:       "(file_.*)".to_string(),
-    //                       replacement: Some("changed-${1}".to_string()),
-    //                       recurse:     false,
-    //                       test_run:    false, };
-    //     app(&args);
-    //     println!("temp: {:?}", temp_dir);
+        // run fresh
+        let args = Args { regex:       "(file.*)".to_string(),
+                          replacement: Some("changed-${1}".to_string()),
+                          recurse:     true,
+                          test_run:    false, };
+        app(&args)?;
+        println!("temp: {:?}", temp_dir);
 
-    //     assert!(temp_dir.path().join("changed-file_0a.txt").exists());
-    //     assert!(temp_dir.path().join("changed-file_0b.txt").exists());
-    //     assert!(temp_dir.path().join("changed-file_0c.txt").exists());
-    //     assert!(temp_dir.path().join("changed-dir_1").exists());
-    //     assert!(temp_dir.path().join("changed-dir_2").exists());
+        assert!(temp_dir.path().join("changed-file_0a.txt").exists());
+        assert!(temp_dir.path().join("changed-file_0b.txt").exists());
+        assert!(temp_dir.path().join("changed-file_0c.txt").exists());
 
-    //     // // run fresh
-    //     // let args = Args { regex:       "(file_.*)".to_string(),
-    //     //                   replacement: Some("changed-${1}".to_string()),
-    //     //                   recurse:     true,
-    //     //                   test_run:    false, };
-    //     // app(&args);
-    //     // println!("temp: {:?}", temp_dir);
+        assert!(temp_dir.path().join("changed-file_0c.txt").exists());
+        assert!(temp_dir.path().join("changed-file_0c.txt").exists());
+        assert!(temp_dir.path().join("dir_1").join("changed-file_1a.txt").exists());
+        assert!(temp_dir.path().join("dir_1").join("dir_11").join("changed-file_11a.txt").exists());
+        assert!(temp_dir.path().join("dir_1").join("dir_11").join("dir_111").join("changed-file_111a.txt").exists());
 
-    //     // assert!(temp_dir.path().join("changed-file_0a.txt").exists());
-    //     // assert!(temp_dir.path().join("changed-file_0b.txt").exists());
-    //     // assert!(temp_dir.path().join("changed-file_0c.txt").exists());
-    //     // assert!(temp_dir.path().join("changed-dir_1").exists());
-    //     // assert!(temp_dir.path().join("changed-dir_2").exists());
+        // run against dirs
+        let args = Args { regex:       "(dir.*)".to_string(),
+                          replacement: Some("changed-${1}".to_string()),
+                          recurse:     true,
+                          test_run:    false, };
+        app(&args)?;
+        println!("temp: {:?}", temp_dir);
 
-    //     temp_dir.close()?;
-    //     Ok(())
-    // }
+        assert!(temp_dir.path().join("changed-file_0a.txt").exists());
+        assert!(temp_dir.path().join("changed-file_0b.txt").exists());
+        assert!(temp_dir.path().join("changed-file_0c.txt").exists());
 
-    // #[test]
-    // fn test_app_with_yesrecursion() -> Result<()> {
-    //     let temp_dir = utility_test_dir_gen()?;
-    //     std::env::set_current_dir(&temp_dir.path())?;
+        assert!(temp_dir.path().join("changed-file_0c.txt").exists());
+        assert!(temp_dir.path().join("changed-file_0c.txt").exists());
+        assert!(temp_dir.path().join("changed-dir_1").join("changed-file_1a.txt").exists());
+        assert!(temp_dir.path().join("changed-dir_1").join("changed-dir_11").join("changed-file_11a.txt").exists());
+        assert!(temp_dir.path()
+                        .join("changed-dir_1")
+                        .join("changed-dir_11")
+                        .join("changed-dir_111")
+                        .join("changed-file_111a.txt")
+                        .exists());
 
-    //     let args = Args { regex:       ".*".to_string(),
-    //                       replacement: Some("changed-${1}".to_string()),
-    //                       recurse:     false,
-    //                       test_run:    false, };
-    //     app(&args);
-    //     println!("temp: {:?}", temp_dir);
+        // run against both
+        let args = Args { regex:       r"(\d+)".to_string(),
+                          replacement: Some("d${1}".to_string()),
+                          recurse:     true,
+                          test_run:    false, };
+        app(&args)?;
+        println!("temp: {:?}", temp_dir);
 
-    //     assert!(temp_dir.path().join("changed-01.txt").exists());
-    //     assert!(temp_dir.path().join("changed-02.txt").exists());
+        assert!(temp_dir.path().join("changed-file_d0a.txt").exists());
+        assert!(temp_dir.path().join("changed-file_d0b.txt").exists());
+        assert!(temp_dir.path().join("changed-file_d0c.txt").exists());
 
-    //     // TODO (WARN): this part of a very real bug -- walkdir is an image of the past
-    //     //      and update will update a directory and then the directory in another file
-    //     //      but the file it tries to update doesn't exist, the updated version did
-    //     //      ... we can get around this by taking advantage of the tree structure of the fs
-    //     //      (if we don't follow symlinks or anything that would loop us)
-    //     //      AND we only look at the last element of the path
-    //     //      THEN we can walk from bottom up
-    //     assert!(temp_dir.path().join("dir_1").join("changed-1a.txt").exists());
-    //     temp_dir.close()?;
-    //     Ok(())
-    // }
-
-    // #[test]
-    // fn test_non_recursion() -> Result<()> {
-    //     let temp_dir = setup_nested_files().unwrap();
-
-    //     let args = Args { regex:       r"file.(\d+)\.txt".to_string(),
-    //                       replacement: Some("changed-${1}.txt".to_string()),
-    //                       recurse:     false,
-    //                       test_run:    false, };
-
-    //     let boop = app(&args);
-    //     dbg!(&boop);
-
-    //     assert!(temp_dir.path().join("changed-01.txt").exists());
-    //     assert!(temp_dir.path().join("changed-02.txt").exists());
-    //     // These files should not be renamed because recursion is disabled
-    //     assert!(!temp_dir.path().join("dir1/changed-1a.txt").exists());
-    //     assert!(!temp_dir.path().join("dir1/dir11/changed-11b.txt").exists());
-    //     Ok(())
-    // }
+        assert!(temp_dir.path().join("changed-file_d0c.txt").exists());
+        assert!(temp_dir.path().join("changed-file_d0c.txt").exists());
+        assert!(temp_dir.path().join("changed-dir_d1").join("changed-file_d1a.txt").exists());
+        assert!(temp_dir.path().join("changed-dir_d1").join("changed-dir_d11").join("changed-file_d11a.txt").exists());
+        assert!(temp_dir.path()
+                        .join("changed-dir_d1")
+                        .join("changed-dir_d11")
+                        .join("changed-dir_d111")
+                        .join("changed-file_d111a.txt")
+                        .exists());
+        Ok(())
+    }
 }
